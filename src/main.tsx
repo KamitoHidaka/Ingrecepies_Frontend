39b2028e9922b4a9f6c72d97658c56d423ff58c0
KamitoHidaka <sebascamposdelgado@live.com>
2024-10-01 16:16:22 -0500

    Beta 1.0

---
+++ src/main.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import {Home} from "./pages/home/Home.tsx";
+import {Login} from "./pages/login/Login.tsx";
+
+const router = createBrowserRouter ([
+
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/login",
+        element: <Login/>
+      }
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,12 @@
+import "./Login.css";
+export const Login = () => {
+  return (
+    <>
+      <form action="" method="post">
+        <input type="text" placeholder="Username or Email" />
+        <input type="password" placeholder="Password" />
+        <input type="submit" value="Login" />
+      </form>
+    </>
+  );
+};
